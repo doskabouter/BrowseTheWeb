4ab8f2c91eaebba7eca3c343522024ae406ad9e0
doskabouter <doskabouter@dd4d296b-e624-0410-81d7-d1d15de9f315>
2012-04-08 15:18:09 +0000

    fixed missing some bookmark thumbs

--- trunk/plugins/BrowseTheWeb/Source/BrowseTheWeb/Bookmark.cs
+++ trunk/plugins/BrowseTheWeb/Source/BrowseTheWeb/Bookmark.cs
@@ -35,238 +35,236 @@ using System.Drawing;
 
 namespace BrowseTheWeb
 {
-  public class Bookmark
-  {
-    private static XmlTextWriter textWriter;
-
-    public static bool Save(TreeView Treeview, string Path)
+    public class Bookmark
     {
-      bool result = false;
+        private static XmlTextWriter textWriter;
 
-      try
-      {
-        textWriter = new XmlTextWriter(Path, null);
-        textWriter.Formatting = Formatting.Indented;
-
-        textWriter.WriteStartDocument();
-        textWriter.WriteStartElement("Bookmarks");
-
-        foreach (TreeNode t in Treeview.Nodes[0].Nodes)
+        public static bool Save(TreeView Treeview, string Path)
         {
-          BookmarkElement bkm = (BookmarkElement)t.Tag;
-          if (bkm != null)
-          {
-            WriteOneEntry(bkm);
+            bool result = false;
 
-            foreach (TreeNode sub in t.Nodes)
+            try
             {
-              BookmarkElement bkm2 = (BookmarkElement)sub.Tag;
-              WriteOneEntry(bkm2);
+                textWriter = new XmlTextWriter(Path, null);
+                textWriter.Formatting = Formatting.Indented;
+
+                textWriter.WriteStartDocument();
+                textWriter.WriteStartElement("Bookmarks");
+
+                foreach (TreeNode t in Treeview.Nodes[0].Nodes)
+                {
+                    BookmarkElement bkm = (BookmarkElement)t.Tag;
+                    if (bkm != null)
+                    {
+                        WriteOneEntry(bkm);
+
+                        foreach (TreeNode sub in t.Nodes)
+                        {
+                            BookmarkElement bkm2 = (BookmarkElement)sub.Tag;
+                            WriteOneEntry(bkm2);
+                        }
+                    }
+                }
+
+                textWriter.WriteEndElement();
+
+                textWriter.WriteEndDocument();
+                textWriter.Close();
+
+                result = true;
             }
-          }
+            catch
+            {
+                // error
+            }
+            finally
+            {
+                if (textWriter != null) textWriter.Close();
+            }
+
+            return result;
+        }
+        public static void Load(TreeView Treeview, string Path)
+        {
+            Treeview.Nodes.Clear();
+
+            TreeNode main = Treeview.Nodes.Add("Bookmarks", "Bookmarks");
+            main.ImageIndex = 2;
+            main.SelectedImageIndex = 2;
+
+            try
+            {
+                BookmarkXml.LoadBookmarks(Path);
+                TreeNode akt = new TreeNode();
+
+                foreach (BookmarkElement bkm in BookmarkXml.BookmarkItems)
+                {
+                    if (bkm.isFolder)
+                    {
+                        akt = main.Nodes.Add(bkm.Name);
+                        akt.Tag = bkm;
+                        akt.ImageIndex = 1;
+                        akt.SelectedImageIndex = 1;
+                    }
+                    if (bkm.isSubFolder)
+                    {
+                        TreeNode sub = akt.Nodes.Add(bkm.Name);
+                        sub.Tag = bkm;
+                    }
+                    if ((!bkm.isFolder) && (!bkm.isSubFolder))
+                    {
+                        TreeNode add = main.Nodes.Add(bkm.Name);
+                        add.Tag = bkm;
+                    }
+                }
+
+                Treeview.Invalidate();
+
+            }
+            catch { }
         }
 
-        textWriter.WriteEndElement();
-
-        textWriter.WriteEndDocument();
-        textWriter.Close();
-
-        result = true;
-      }
-      catch
-      {
-        // error
-      }
-      finally
-      {
-        if (textWriter != null) textWriter.Close();
-      }
-
-      return result;
-    }
-    public static void Load(TreeView Treeview, string Path)
-    {
-      Treeview.Nodes.Clear();
-
-      TreeNode main = Treeview.Nodes.Add("Bookmarks", "Bookmarks");
-      main.ImageIndex = 2;
-      main.SelectedImageIndex = 2;
-
-      try
-      {
-        BookmarkXml.LoadBookmarks(Path);
-        TreeNode akt = new TreeNode();
-
-        foreach (BookmarkElement bkm in BookmarkXml.BookmarkItems)
+        private static void WriteOneEntry(BookmarkElement bkm)
         {
-          if (bkm.isFolder)
-          {
-            akt = main.Nodes.Add(bkm.Name);
-            akt.Tag = bkm;
-            akt.ImageIndex = 1;
-            akt.SelectedImageIndex = 1;
-          }
-          if (bkm.isSubFolder)
-          {
-            TreeNode sub = akt.Nodes.Add(bkm.Name);
-            sub.Tag = bkm;
-          }
-          if ((!bkm.isFolder) && (!bkm.isSubFolder))
-          {
-            TreeNode add = main.Nodes.Add(bkm.Name);
-            add.Tag = bkm;
-          }
+            textWriter.WriteStartElement("Entry");
+
+            textWriter.WriteStartElement("Name");
+            textWriter.WriteValue(bkm.Name);
+            textWriter.WriteEndElement();
+
+            textWriter.WriteStartElement("URL");
+            textWriter.WriteValue(bkm.Url);
+            textWriter.WriteEndElement();
+
+            textWriter.WriteStartElement("Visited");
+            textWriter.WriteValue(bkm.Visited);
+            textWriter.WriteEndElement();
+
+            textWriter.WriteStartElement("LastVisited");
+            textWriter.WriteValue(bkm.LastVisited);
+            textWriter.WriteEndElement();
+
+            textWriter.WriteStartElement("Created");
+            textWriter.WriteValue(bkm.Created);
+            textWriter.WriteEndElement();
+
+            textWriter.WriteStartElement("isFolder");
+            textWriter.WriteValue(bkm.isFolder);
+            textWriter.WriteEndElement();
+
+            textWriter.WriteStartElement("isSubFolder");
+            textWriter.WriteValue(bkm.isSubFolder);
+            textWriter.WriteEndElement();
+
+            textWriter.WriteEndElement();
+
         }
 
-        Treeview.Invalidate();
-
-      }
-      catch { }
-    }
-
-    private static void WriteOneEntry(BookmarkElement bkm)
-    {
-      textWriter.WriteStartElement("Entry");
-
-      textWriter.WriteStartElement("Name");
-      textWriter.WriteValue(bkm.Name);
-      textWriter.WriteEndElement();
-
-      textWriter.WriteStartElement("URL");
-      textWriter.WriteValue(bkm.Url);
-      textWriter.WriteEndElement();
-
-      textWriter.WriteStartElement("Visited");
-      textWriter.WriteValue(bkm.Visited);
-      textWriter.WriteEndElement();
-
-      textWriter.WriteStartElement("LastVisited");
-      textWriter.WriteValue(bkm.LastVisited);
-      textWriter.WriteEndElement();
-
-      textWriter.WriteStartElement("Created");
-      textWriter.WriteValue(bkm.Created);
-      textWriter.WriteEndElement();
-
-      textWriter.WriteStartElement("isFolder");
-      textWriter.WriteValue(bkm.isFolder);
-      textWriter.WriteEndElement();
-
-      textWriter.WriteStartElement("isSubFolder");
-      textWriter.WriteValue(bkm.isSubFolder);
-      textWriter.WriteEndElement();
-
-      textWriter.WriteEndElement();
-
-    }
-
-    public static bool Exists(TreeView Treeview, string Name)
-    {
-      foreach (TreeNode t in Treeview.Nodes[0].Nodes)
-      {
-        if (Name == t.Text)
-          return true;
-        foreach (TreeNode sub in t.Nodes)
+        public static bool Exists(TreeView Treeview, string Name)
         {
-          if (Name == sub.Text)
+            foreach (TreeNode t in Treeview.Nodes[0].Nodes)
+            {
+                if (Name == t.Text)
+                    return true;
+                foreach (TreeNode sub in t.Nodes)
+                {
+                    if (Name == sub.Text)
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool isValidUrl(string URL)
+        {
+            try
+            {
+                Uri urlCheck = new Uri(URL);
+                WebRequest request = WebRequest.Create(urlCheck);
+                request.Timeout = 10000;
+
+                WebResponse response;
+
+                response = request.GetResponse();
+            }
+            catch (Exception)
+            {
+                return false; //url does not exist
+            }
             return true;
         }
-      }
-      return false;
-    }
 
-    public static bool isValidUrl(string URL)
-    {
-      try
-      {
-        Uri urlCheck = new Uri(URL);
-        WebRequest request = WebRequest.Create(urlCheck);
-        request.Timeout = 10000;
-
-        WebResponse response;
-
-        response = request.GetResponse();
-      }
-      catch (Exception)
-      {
-        return false; //url does not exist
-      }
-      return true;
-    }
-
-    public static void SaveSnap(Bitmap Snap, string Url)
-    {
-      try
-      {
-        string filename = GetThumbString(Url);
-        filename = Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb\\" + filename;
-
-        Snap.Save(filename);
-      }
-      catch { }
-    }
-    public static Bitmap GetSnap(string Url)
-    {
-      Bitmap snap = null;
-
-      try
-      {
-        string filename = GetThumbString(Url);
-        filename = Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb\\" + filename;
-
-        if (File.Exists(filename))
+        public static void SaveSnap(Bitmap Snap, string Url)
         {
-          snap = (Bitmap)Bitmap.FromFile(filename);
-          return snap;
+            try
+            {
+                string filename = GetThumbString(Url);
+                filename = Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb\\" + filename;
+
+                Snap.Save(filename);
+            }
+            catch { }
         }
-      }
-      catch { }
-
-      return snap;
-    }
-
-    public static string GetSnapPath(string Url)
-    {
-      string filename = GetThumbString(Url);
-      filename = Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb\\" + filename;
-
-      return filename;
-    }
-    public static void InitCachePath()
-    {
-      if (!Directory.Exists(Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb"))
-        Directory.CreateDirectory(Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb");
-
-
-      if (Directory.Exists(Config.GetFolder(MediaPortal.Configuration.Config.Dir.Cache) + "\\BrowseTheWeb"))
-      {
-        string[] files = Directory.GetFiles(Config.GetFolder(MediaPortal.Configuration.Config.Dir.Cache) + "\\BrowseTheWeb", "*.*");
-        foreach (string f in files)
+        public static Bitmap GetSnap(string Url)
         {
-          File.Move(f, Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb\\" + Path.GetFileName(f));
+            Bitmap snap = null;
+
+            try
+            {
+                string filename = GetThumbString(Url);
+                filename = Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb\\" + filename;
+
+                if (File.Exists(filename))
+                {
+                    snap = (Bitmap)Bitmap.FromFile(filename);
+                    return snap;
+                }
+            }
+            catch { }
+
+            return snap;
         }
-      }
 
+        public static string GetSnapPath(string Url)
+        {
+            string filename = GetThumbString(Url);
+            filename = Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb\\" + filename;
+
+            return filename;
+        }
+        public static void InitCachePath()
+        {
+            if (!Directory.Exists(Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb"))
+                Directory.CreateDirectory(Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb");
+
+
+            if (Directory.Exists(Config.GetFolder(MediaPortal.Configuration.Config.Dir.Cache) + "\\BrowseTheWeb"))
+            {
+                string[] files = Directory.GetFiles(Config.GetFolder(MediaPortal.Configuration.Config.Dir.Cache) + "\\BrowseTheWeb", "*.*");
+                foreach (string f in files)
+                {
+                    File.Move(f, Config.GetFolder(MediaPortal.Configuration.Config.Dir.Thumbs) + "\\BrowseTheWeb\\" + Path.GetFileName(f));
+                }
+            }
+
+        }
+
+        private static string GetThumbString(string Name)
+        {
+            string result = Name;
+
+            if (result.EndsWith("/")) result = result.Substring(0, result.Length - 1);
+
+            int x = result.IndexOf("//");
+            if (x > 0)
+            {
+                result = result.Substring(x + 2);
+            }
+
+            foreach (char c in Path.GetInvalidFileNameChars())
+                result = result.Replace(c, '_');
+            result = result + ".png";
+            return result;
+        }
     }
-
-    private static string GetThumbString(string Name)
-    {
-      string result = Name;
-
-      if (result.EndsWith("/")) result = result.Substring(0, result.Length - 1);
-
-      int x = result.IndexOf("//");
-      if (x > 0)
-      {
-        result = result.Substring(x + 2);
-      }
-
-      result = result.Replace("/", "_");
-      result = result.Replace(".", "_");
-      result = result.Replace("?", "_");
-      result = result + ".png";
-
-      return result;
-    }
-  }
 }
